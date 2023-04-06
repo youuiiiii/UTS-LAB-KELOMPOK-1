36f7100c660e755af49bcff2a73558ef334b7c16
haruza123 <haruzakatana@gmail.com>
2023-04-06 11:05:58 +0700

    baru

--- ini gamenya/tapgame.js
+++ ini gamenya/tapgame.js
@@ -1,100 +1,115 @@
-// Variables
-let money = 0;
-let clickValue = 1;
-let autoClickers = 0;
-let autoClickerValue = 10;
-let cabangOpened = false;
-let cabangValue = 2;
-let upgraded = false;
-let upgradeValue = 2;
+const moraDisplay = document.getElementById('mora');
+const drinkButton = document.getElementById("drink");
+const snackButton = document.getElementById("snack");
+const toyButton = document.getElementById("toy");
+const machinesButton = document.getElementById("buymachines");
+const autoitemButton = document.getElementById("autoitem");
+const autoclickButton = document.getElementById("autoclick");
+const machine = document.querySelector('#machines button');
+ let mora = 0;
+let drink = 10;
+let snack = 10;
+let toy = 10;
+let machines = 1;
+let machinePrice = 1000;
+let autoitemPrice = 500;
+let autoclickPrice = 200;
 
-
-// Elements
-const moneyElement = document.getElementById("money");
-const tahuElement = document.getElementById("machines").querySelector("button");
-const autoClickersElement = document.getElementById("autoclick").querySelector("button");
-const buyClickerElement = document.getElementById("autoitem").querySelector("button");
-const bukaCabangElement = document.getElementById("buymachines").querySelector("button");
-const upgradeElement = document.getElementById("drink");
-const upgradeElement1 = document.getElementById("snack");
-const upgradeElement2 = document.getElementById("toy");
-
-// Clicking Tahu
-tahuElement.addEventListener("click", () => {
-  money += clickValue;
-  moneyElement.innerText = money;
-});
-
-// Buying Auto-Clickers
-buyClickerElement.addEventListener("click", () => {
-  if (money < autoClickerValue) {
-    alert("Uang tidak cukup!");
-    return;
+ drinkButton.addEventListener('click', function () {
+  if (mora >= 1) {
+    mora -= 1;
+    drink += 1;
+    updateDisplay();
   }
-
-  money -= autoClickerValue;
-  autoClickers++;
-  moneyElement.innerText = money;
-  autoClickersElement.innerText = "Auto Clickers: " + autoClickers;
-
-  setInterval(() => {
-    money += autoClickers;
-    moneyElement.innerText = money;
-  }, 1000);
 });
 
-// Opening Cabang
-bukaCabangElement.addEventListener("click", () => {
-  if (money < 500 || cabangOpened) {
-    alert("Cabang telah dibuka atau uang tidak cukup!");
-    return;
+ snackButton.addEventListener('click', function () {
+  if (mora >= 2) {
+    mora -= 2;
+    snack += 1;
+    updateDisplay();
   }
-
-  money -= 500;
-  cabangOpened = true;
-  cabangValue *= 2;
-  moneyElement.innerText = money;
 });
 
-// Upgrading Click Value
-upgradeElement.addEventListener("click", () => {
-  if (money < 10 || upgraded) {
-    alert("uang tidak cukup!");
-    return;
+ toyButton.addEventListener('click', function () {
+  if (mora >= 3) {
+    mora -= 3;
+    toy += 1;
+    updateDisplay();
   }
-
-  money -= 10;
-  upgradeValue = 2;
-  clickValue += upgradeValue;
-  autoClickerValue += upgradeValue;
-  cabangValue += upgradeValue;
-  moneyElement.innerText = money;
 });
 
-upgradeElement1.addEventListener("click", () => {
-    if (money < 10 || upgraded) {
-      alert("uang tidak cukup!");
-      return;
-    }
-  
-    money -= 10;
-    upgradeValue = 2;
-    clickValue += upgradeValue;
-    autoClickerValue += upgradeValue;
-    cabangValue += upgradeValue;
-    moneyElement.innerText = money;
-  });
+ machinesButton.addEventListener('click', function () {
+  if (mora >= machinePrice) {
+    mora -= machinePrice;
+    machines += 1;
+    machinePrice *= 1.2;
+    updateDisplay();
+  }
+});
 
-  upgradeElement2.addEventListener("click", () => {
-    if (money < 10 || upgraded) {
-      alert("uang tidak cukup!");
-      return;
-    }
-  
-    money -= 10;
-    upgradeValue = 2;
-    clickValue += upgradeValue;
-    autoClickerValue += upgradeValue;
-    cabangValue += upgradeValue;
-    moneyElement.innerText = money;
-  });
+ autoitemButton.addEventListener('click', function () {
+  if (mora >= autoitemPrice) {
+    mora -= autoitemPrice;
+    setInterval(() => {
+      if (mora >= 1) {
+        mora -= 1;
+        drink += 1;
+      }
+      if (mora >= 2) {
+        mora -= 2;
+        snack += 1;
+      }
+      if (mora >= 3) {
+        mora -= 3;
+        toy += 1;
+      }
+      updateDisplay();
+    }, 1000);
+    autoitemPrice *= 1.5;
+    updateDisplay();
+  }
+});
+
+ autoclickButton.addEventListener('click', function () {
+  if (mora >= autoclickPrice) {
+    mora -= autoclickPrice;
+    setInterval(() => {
+      collectFromMachine();
+      updateDisplay();
+    }, 1000);
+    autoclickPrice *= 1.5;
+    updateDisplay();
+  }
+});
+
+ machine.addEventListener('click', function () {
+  collectFromMachine();
+  updateDisplay();
+});
+
+ function collectFromMachine() {
+  if (drink > 0) {
+    drink -= 1;
+    mora += 2 * machines;
+  }
+  if (snack > 0) {
+    snack -= 1;
+    mora += 4 * machines;
+  }
+  if (toy > 0) {
+    toy -= 1;
+    mora += 6 * machines;
+  }
+}
+
+ function updateDisplay() {
+  moraDisplay.textContent = mora;
+  document.getElementById('totaldrink').textContent = drink;
+  document.getElementById('totalsnack').textContent = snack;
+  document.getElementById('totaltoy').textContent = toy;
+  document.getElementById('machinesprice').textContent = Math.floor(machinePrice);
+  document.getElementById('autoitemprice').textContent = Math.floor(autoitemPrice);
+  document.getElementById('autoclickprice').textContent = Math.floor(autoclickPrice);
+}
+ updateDisplay();
